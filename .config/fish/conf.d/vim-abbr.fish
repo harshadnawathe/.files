4eaba9f07fa820a2b3313e4483fcc7a0674c126a
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-16 08:04:38 +0530

    Fish - Update vim abbreviations

--- .config/fish/conf.d/vim-abbr.fish
+++ .config/fish/conf.d/vim-abbr.fish
@@ -1,6 +1,5 @@
-abbr -a v --function edit_file_command_fzf
+abbr -a v --function vim_edit_file_command_fzf
+abbr -a e --function edit_file_command_fzf
 abbr -a vim nvim
-abbr -a vps nvim +PackerSync
-abbr -a gv neovide
 abbr -a nvcfg nvim ~/.config/nvim/lua/user/init.lua
 abbr -a avim NVIM_APPNAME=nvim.astro nvim

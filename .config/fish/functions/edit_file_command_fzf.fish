f38f8fc20f3353b07473bb2a460211f3d3844c8a
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-16 08:03:19 +0530

    Fish - Use select_file script in edit_file_command_fzf

--- .config/fish/functions/edit_file_command_fzf.fish
+++ .config/fish/functions/edit_file_command_fzf.fish
@@ -1,19 +1,9 @@
 function edit_file_command_fzf -d "Select and open file using default editor"
-  set file $(
-  FZF_DEFAULT_COMMAND='fd --hidden --type=f --color=always --exclude=".git"' fzf --ansi --query="$1" --no-multi --select-1 --exit-0 \
-    --margin=1 --height=~50% --padding=1 --reverse --border \
-    --preview 'bat --style=plain --color=always --line-range :500 {}' \
-    --bind ctrl-y:preview-up \
-    --bind ctrl-e:preview-down \
-    --bind ctrl-b:preview-page-up \
-    --bind ctrl-f:preview-page-down \
-    --bind ctrl-u:preview-half-page-up \
-    --bind ctrl-d:preview-half-page-down
-  )
+    set open_cmd $(~/bin/fzf_select_file | awk -F':' -v e=$EDITOR '{ printf "%s %s", e, $1 }')
 
-  if test -n "$file"
-    echo $EDITOR $file
-  else
-    echo $EDITOR
-  end
+    if test -n "$open_cmd"
+        echo $open_cmd
+    else
+        echo $EDITOR
+    end
 end

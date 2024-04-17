005a51ba55199c9012610f26989a9b5b34a90983
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-17 06:51:59 +0530

    Fish - Use catppuccin mocha colors for FZF

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,18 +3,24 @@ eval (/opt/homebrew/bin/brew shellenv)
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
-    
+
     # # Abbreaviations
     # for f in $(find ~/.config/fish/abbrs/ -type f -name '*-abbr.fish' )
     #     source $f
     # end
-    
+
     # Use lsd as ls command with color and icons
     set -g __fish_ls_command lsd
     set -g __fish_ls_color_opt --color=auto
 
     #Theme
     # fish_config theme choose Nord
+
+    # FZF Theme
+    set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
     starship init fish | source
     zoxide init fish | source
@@ -32,7 +38,7 @@ export GOPATH=/Users/harshadn/Go
 
 fish_add_path -m \
     $GOPATH/bin \
-    ~/bin  \
+    ~/bin \
     /usr/sbin/ \
     "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
 

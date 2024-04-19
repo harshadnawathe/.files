30d0e9f8f574cdbd93b080f152800d9fda92cb2a
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-19 18:36:12 +0530

    NVim - Use Aerial plugin from Lazy Extra

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     -- import/override with your plugins
     { import = "plugins" },
   },

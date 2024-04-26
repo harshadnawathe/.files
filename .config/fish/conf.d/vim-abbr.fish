e776f6bf804ac36588d26af09bf237c4b94b1724
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-26 10:22:31 +0530

    Fish - Remove abbr vim

--- .config/fish/conf.d/vim-abbr.fish
+++ .config/fish/conf.d/vim-abbr.fish
@@ -1,5 +1,4 @@
 abbr -a v --function vim_edit_file_command_fzf
 abbr -a e --function edit_file_command_fzf
-abbr -a vim nvim
 abbr -a nvcfg nvim ~/.config/nvim/lua/user/init.lua
 abbr -a avim NVIM_APPNAME=nvim.astro nvim

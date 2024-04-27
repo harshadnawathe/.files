f24d7d5b9291b53380d48e80566b26bd838d36d5
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-04-27 20:13:49 +0530

    NVim - Extend catppuccin configuration from LazyVim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,13 +2,15 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    opts = {
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "light",
-        percentage = 0.9, -- percentage of the shade to apply to the inactive window
-      },
-    },
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        dim_inactive = {
+          enabled = true, -- dims the background color of inactive window
+          shade = "light",
+          percentage = 0.9, -- percentage of the shade to apply to the inactive window
+        },
+      })
+    end,
   },
   {
     "LazyVim/LazyVim",

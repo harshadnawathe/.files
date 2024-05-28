ca32e94ccf5d9fe7c52eae5d76f626cab2027d90
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-05-28 14:51:01 +0530

    NVim - Add mini-surround plugin

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -28,10 +28,13 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
-    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.navic" },
+
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },

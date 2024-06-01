f176a21e6db7ff5379c13341463632bf728ac990
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-01 11:58:05 +0530

    Fish - Minor refactor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,46 +1,42 @@
 eval (/opt/homebrew/bin/brew shellenv)
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -U fish_greeting
+  # Commands to run in interactive sessions can go here
+  set -U fish_greeting
 
-    # # Abbreaviations
-    # for f in $(find ~/.config/fish/abbrs/ -type f -name '*-abbr.fish' )
-    #     source $f
-    # end
+  # # Abbreaviations
+  # for f in $(find ~/.config/fish/abbrs/ -type f -name '*-abbr.fish' )
+  #     source $f
+  # end
 
-    # Use lsd as ls command with color and icons
-    set -g __fish_ls_command lsd
-    set -g __fish_ls_color_opt --color=auto
+  # Use lsd as ls command with color and icons
+  set -g __fish_ls_command lsd
+  set -g __fish_ls_color_opt --color=auto
 
-    #Theme
-    # fish_config theme choose Nord
+  #Theme
+  # fish_config theme choose Nord
 
-    # FZF Theme
-    set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+  # FZF Theme
+  set -Ux FZF_DEFAULT_OPTS "\
+    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+    --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-    starship init fish | source
-    zoxide init fish | source
+  starship init fish | source
+  zoxide init fish | source
+  mise activate fish | source
+else
+  mise activate --shims | source
 end
-
-
-#Add direnvj
-# direnv hook fish | source
-
-#rtx 
-mise activate fish | source
 
 # Go
 export GOPATH=/Users/harshadn/Go
 
 fish_add_path -m \
-    $GOPATH/bin \
-    ~/bin \
-    /usr/sbin/ \
-    "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
+  $GOPATH/bin \
+  ~/bin \
+  /usr/sbin/ \
+  "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 # set --export --prepend PATH "/Users/harshadn/.rd/bin"

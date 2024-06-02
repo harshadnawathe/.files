be7e71a5e2b6641f47d229fe2ba0f2341d16f863
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-02 10:55:28 +0530

    Remove background from FZF

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,34 @@
 eval (/opt/homebrew/bin/brew shellenv)
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  set -U fish_greeting
+    # Commands to run in interactive sessions can go here
+    set -U fish_greeting
 
-  # # Abbreaviations
-  # for f in $(find ~/.config/fish/abbrs/ -type f -name '*-abbr.fish' )
-  #     source $f
-  # end
+    # # Abbreaviations
+    # for f in $(find ~/.config/fish/abbrs/ -type f -name '*-abbr.fish' )
+    #     source $f
+    # end
 
-  # Use lsd as ls command with color and icons
-  set -g __fish_ls_command lsd
-  set -g __fish_ls_color_opt --color=auto
+    # Use lsd as ls command with color and icons
+    set -g __fish_ls_command lsd
+    set -g __fish_ls_color_opt --color=auto
 
-  #Theme
-  # fish_config theme choose Nord
+    #Theme
+    # fish_config theme choose Nord
 
-  # FZF Theme
-  set -Ux FZF_DEFAULT_OPTS "\
-    --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+    #--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+
+    # FZF Theme
+    set -Ux FZF_DEFAULT_OPTS "\
+    --color=spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-  starship init fish | source
-  zoxide init fish | source
-  mise activate fish | source
+    starship init fish | source
+    zoxide init fish | source
+    mise activate fish | source
 else
-  mise activate --shims | source
+    mise activate --shims | source
 end
 
 # golang
@@ -35,9 +37,9 @@ fish_add_path $GOPATH/bin
 
 # custom tools
 fish_add_path -m \
-  ~/bin \
-  /usr/sbin/ \
-  "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
+    ~/bin \
+    /usr/sbin/ \
+    "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
 
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 # set --export --prepend PATH "/Users/harshadn/.rd/bin"

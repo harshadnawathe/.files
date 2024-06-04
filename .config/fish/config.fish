a6f9cb3fbf81dd182a06e3018cd3fc72f4d8e662
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-04 21:45:35 +0530

    Fish - Refactor fish configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,6 @@
-## golang
-#set -x GOPATH (~/.local/share/mise/shims/go env GOPATH)
-#fish_add_path -gP $GOPATH/bin
 
-# custom tools
-fish_add_path -gP \
+fish_add_path -gm \
   ~/bin \
+  ~/go/bin \
   "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
+

ee4bbd7e2ea81390442d4c1f9feb989b083fbe98
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-06 21:36:10 +0530

    NVim - Add json-lsp to mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -34,6 +34,9 @@ return {
           "github:nvim-java/mason-registry",
           "github:mason-org/mason-registry",
         },
+        ensure_installed = {
+          "json-lsp",
+        },
       })
     end,
   },

47510ffb4dce5cbe3486652b44b4f0c13ede5e39
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-26 10:50:54 +0530

    NVim - Add telescope through LazyExtras

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.navic" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
 
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
 

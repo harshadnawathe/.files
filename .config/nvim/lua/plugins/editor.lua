a3754c6e8fc0662640462be7ea82434dc633437c
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-26 09:44:15 +0530

    NVim - Open Trouble LSP references in the bottom

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -163,4 +163,14 @@ return {
   {
     "tpope/vim-rsi",
   },
+  {
+    "folke/trouble.nvim",
+    keys = {
+      {
+        "<leader>cS",
+        "<cmd>Trouble lsp toggle focus=false win.position=bottom<cr>",
+        desc = "LSP references/definitions/... (Trouble)",
+      },
+    },
+  },
 }

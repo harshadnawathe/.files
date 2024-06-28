1fa3dd200c4ff11a5277430c9cb81deea6ea4f3f
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-06-28 18:25:40 +0530

    NVim: fix(snippets): Use LuaSnip from LazyExtra to enable custom snippets.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -52,23 +52,11 @@ return {
         },
       }
     end,
-    init = function()
+    opts = function(_, _)
+      -- register custom snippets
       local nvim_config_path = vim.fn.stdpath("config") .. "/snippets"
       require("luasnip.loaders.from_vscode").lazy_load({ paths = { nvim_config_path .. "/vscode/" } })
       require("luasnip.loaders.from_lua").lazy_load({ paths = { nvim_config_path .. "/lua/" } })
-
-      vim.api.nvim_create_autocmd("ModeChanged", {
-        pattern = "*",
-        callback = function()
-          if
-            ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
-            and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-            and not require("luasnip").session.jump_active
-          then
-            require("luasnip").unlink_current()
-          end
-        end,
-      })
     end,
   },
   {

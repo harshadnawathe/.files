f617048c0df6efe1b37f1f168fd706b61278497c
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-07-05 15:56:59 +0530

    feat(nvim): use copilot-chat plugin from Lazy Extra

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,8 +20,9 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
-    { import = "lazyvim.plugins.extras.coding.yanky"},
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -8,12 +8,6 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    version = "v2.8.0",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim" },
-      { "nvim-telescope/telescope.nvim" },
-    },
     opts = {
       prompts = {
         Refactor = {
@@ -44,69 +38,26 @@ return {
           prompt = "/COPILOT_GENERATE Please explain how the selected code works, then generate unit tests for it.",
         }
       },
-      window = {
-        width = 0.3,
-      },
-    },
-    commands = {
-      "CopilotChat",
-      "CopilotChatOpen",
-      "CopilotChatClose",
-      "CopilotChatToggle",
-      "CopilotChatStop",
-      "CopilotChatReset",
-      "CopilotChatSave",
-      "CopilotChatLoad",
     },
     cond = function()
       return vim.env.ENABLE_COPILOT ~= nil
     end,
     keys = {
-      { "<leader>cp", desc = "Copilot" },
+      { "<leader>ae", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code", mode = { "n", "x" } },
+      { "<leader>at", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests", mode = { "n", "x" } },
+      { "<leader>ar", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code", mode = { "n", "x" } },
+      { "<leader>aR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code", mode = { "n", "x" } },
       {
-        "<leader>cpq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
-          end
-        end,
-        desc = "CopilotChat - Quick chat",
-      },
-      {
-        "<leader>cph",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.help_actions())
-        end,
-        desc = "CopilotChat - Help actions",
-      },
-      {
-        "<leader>cpp",
-        function()
-          local actions = require("CopilotChat.actions")
-          require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
-        end,
-        desc = "CopilotChat - Prompt actions",
-        mode = { "n", "x" },
-      },
-      { "<leader>cpe", "<cmd>CopilotChatExplain<cr>", desc = "CopilotChat - Explain code", mode = { "n", "x" } },
-      { "<leader>cpt", "<cmd>CopilotChatTests<cr>", desc = "CopilotChat - Generate tests", mode = { "n", "x" } },
-      { "<leader>cpr", "<cmd>CopilotChatReview<cr>", desc = "CopilotChat - Review code", mode = { "n", "x" } },
-      { "<leader>cpR", "<cmd>CopilotChatRefactor<cr>", desc = "CopilotChat - Refactor code", mode = { "n", "x" } },
-      {
-        "<leader>cpm",
+        "<leader>am",
         "<cmd>CopilotChatCommit<cr>",
         desc = "CopilotChat - Generate commit message for all changes",
       },
       {
-        "<leader>cpM",
+        "<leader>aM",
         "<cmd>CopilotChatCommitStaged<cr>",
         desc = "CopilotChat - Generate commit message for staged changes",
       },
-      { "<leader>cpf", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
-      { "<leader>cpl", "<cmd>CopilotChatReset<cr>", desc = "CopilotChat - Clear buffer and chat history" },
-      { "<leader>cpv", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat - Toggle" },
+      { "<leader>af", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "CopilotChat - Fix Diagnostic" },
     },
   },
 }

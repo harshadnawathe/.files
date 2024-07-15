b426b05d6172cc8e6983ba71ceab0a79042c2d36
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-07-15 12:00:43 +0530

    feat(fish): remove abbr v & e

--- .config/fish/conf.d/vim-abbr.fish
+++ .config/fish/conf.d/vim-abbr.fish
@@ -1,4 +1,2 @@
-abbr -a v --function vim_edit_file_command_fzf
-abbr -a e --function edit_file_command_fzf
 abbr -a nvcfg nvim ~/.config/nvim/lua/user/init.lua
 abbr -a avim NVIM_APPNAME=nvim.astro nvim

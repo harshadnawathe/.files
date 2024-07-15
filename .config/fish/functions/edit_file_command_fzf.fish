d53d16a7d00c4174cfb8efc0d7b6723980b9b4d9
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-07-15 12:02:24 +0530

    feat(fish): remove fzf based edit_file functions

--- .config/fish/functions/edit_file_command_fzf.fish
+++
@@ -1,9 +0,0 @@
-function edit_file_command_fzf -d "Select and open file using default editor"
-    set open_cmd $(~/bin/fzf_select_file | awk -F':' -v e=$EDITOR '{ printf "%s %s", e, $1 }')
-
-    if test -n "$open_cmd"
-        echo $open_cmd
-    else
-        echo $EDITOR
-    end
-end

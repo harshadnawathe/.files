36f00a18a719a82691bb5f3eee4fbf55a97d6797
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-08-21 16:04:49 +0530

    feat(wezterm): refactor backgrounds config

--- .config/wezterm/utils/backgrounds.lua
+++ .config/wezterm/utils/backgrounds.lua
@@ -1,5 +1,9 @@
-function expand_home(path) 
-  return string.gsub(path, "^~", os.getenv("HOME"))
+local function expand_home(path)
+	local home = os.getenv("HOME")
+	if not home then
+		return path
+	end
+	return string.gsub(path, "^~", home)
 end
 
 return {
@@ -7,99 +11,97 @@ return {
 		{
 			source = {
 				File = {
-					path = os.getenv("HOME")
-						.. "/.config/wezterm/wallpapers/darth-vader-red-lightsaber-star-wars-moewalls-com.gif",
+					path = expand_home(
+						"~/.config/wezterm/wallpapers/darth-vader-red-lightsaber-star-wars-moewalls-com.gif"
+					),
 				},
 			},
 			horizontal_align = "Center",
 			vertical_align = "Middle",
 		},
-    {
-      source = {
-        Color = "black",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.4,
-    }
+		{
+			source = {
+				Color = "black",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.4,
+		},
 	},
 	["Anime-Raid-in-the-dark"] = {
 		{
 			source = {
 				File = {
-					path = os.getenv("HOME")
-						.. "/.config/wezterm/wallpapers/raid-in-the-dark.png",
+					path = expand_home("~/.config/wezterm/wallpapers/raid-in-the-dark.png"),
 				},
 			},
 			horizontal_align = "Center",
 			vertical_align = "Middle",
 		},
-    {
-      source = {
-        Color = "black",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.7,
-    }
+		{
+			source = {
+				Color = "black",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.7,
+		},
 	},
 	["Batman-Logo-The-Dark-Knight"] = {
 		{
 			source = {
 				File = {
-					path = os.getenv("HOME")
-						.. "/.config/wezterm/wallpapers/batman-logo.jpg",
+					path = expand_home("~/.config/wezterm/wallpapers/batman-logo.jpg"),
 				},
 			},
 			horizontal_align = "Center",
 			vertical_align = "Middle",
 		},
-    {
-      source = {
-        Color = "black",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.6,
-    }
+		{
+			source = {
+				Color = "black",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.6,
+		},
 	},
 	["Black-Sand-Dunes"] = {
 		{
 			source = {
 				File = {
-					path = os.getenv("HOME")
-						.. "/.config/wezterm/wallpapers/pexels-adrien-olichon-1257089-2387793.jpg",
+					path = expand_home("~/.config/wezterm/wallpapers/pexels-adrien-olichon-1257089-2387793.jpg"),
 				},
 			},
 			horizontal_align = "Center",
 			vertical_align = "Middle",
 		},
-    {
-      source = {
-        Color = "black",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.2,
-    }
+		{
+			source = {
+				Color = "black",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.2,
+		},
 	},
-  ["Iron-Man"] = {
-    {
-      source = {
-        File = {
-          path = expand_home("~/.config/wezterm/wallpapers/iron-man-wallhaven-j8xlpy.jpg"),
-        }
-      },
-      horizontal_align = "Center",
-      vertical_align = "Middle",
-    },
-    {
-      source = {
-        Color = "black"
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.6,
-    }
-  },
+	["Iron-Man"] = {
+		{
+			source = {
+				File = {
+					path = expand_home("~/.config/wezterm/wallpapers/iron-man-wallhaven-j8xlpy.jpg"),
+				},
+			},
+			horizontal_align = "Center",
+			vertical_align = "Middle",
+		},
+		{
+			source = {
+				Color = "black",
+			},
+			width = "100%",
+			height = "100%",
+			opacity = 0.6,
+		},
+	},
 }

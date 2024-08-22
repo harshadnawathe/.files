a27c8cff440a21628a781da6682b54387dbabd35
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-08-22 14:26:24 +0530

    feat(nvim): remove mapping configuration for mini.surround

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -16,18 +16,4 @@ return {
       { "<leader>%", ":TSJToggle<CR>", desc = "args: Split or Join" },
     },
   },
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "gSa", -- Add surrounding in Normal and Visual modes
-        delete = "gSd", -- Delete surrounding
-        find = "gSf", -- Find surrounding (to the right)
-        find_left = "gSF", -- Find surrounding (to the left)
-        highlight = "gSh", -- Highlight surrounding
-        replace = "gSr", -- Replace surrounding
-        update_n_lines = "gSn", -- Update `n_lines`
-      },
-    },
-  },
 }

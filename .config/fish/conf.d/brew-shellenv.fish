a2ce3fdc4e928f6ed867e6db7ef11a753d3ae3d4
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-09-16 09:41:30 +0530

    feat(fish): update brew-shellenv script

--- .config/fish/conf.d/brew-shellenv.fish
+++ .config/fish/conf.d/brew-shellenv.fish
@@ -1,6 +1,6 @@
 set -gx HOMEBREW_PREFIX "/opt/homebrew";
 set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
 set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-fish_add_path -gP "/opt/homebrew/bin" "/opt/homebrew/sbin";
+fish_add_path -gmP "/opt/homebrew/bin" "/opt/homebrew/sbin";
 if test -n "$MANPATH[1]"; set -gx MANPATH '' $MANPATH; end;
 if not contains "/opt/homebrew/share/info" $INFOPATH; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;

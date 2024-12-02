ac7102716d0d047237994b350dc3b9cc0470b58a
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-02 21:36:25 +0530

    feat(nvim): use live_grep_glob for Grep

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -43,4 +43,11 @@ return {
       },
     },
   },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>sg", LazyVim.pick("live_grep_glob"), desc = "Grep (Root Dir)" },
+      { "<leader>sG", LazyVim.pick("live_grep_glob", { root = false }), desc = "Grep (cwd)" },
+    },
+  },
 }

02de2fad992d652fe93684d8a31e85f558b58a61
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-10 14:52:57 +0530

    refactor(nvim): clean up config options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,25 +9,3 @@ vim.g.trouble_lualine = true
 vim.o.shell = "/opt/homebrew/bin/fish"
 
 vim.opt.colorcolumn = "120"
-vim.opt.cursorcolumn = false
-vim.opt.cursorline = false
-vim.opt.number = true
-vim.opt.spell = false
-vim.opt.wrap = false
-
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
--- Lazy extra - Python
---
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
-vim.g.lazyvim_python_ruff = "ruff"
-
-vim.g.lazyvim_statuscolumn = {
-  folds_open = true, -- show fold sign when fold is open
-  folds_githl = true, -- highlight fold sign with git sign color
-}

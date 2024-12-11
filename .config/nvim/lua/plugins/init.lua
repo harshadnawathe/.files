e8c5687e880eed58dcda00b4a27b2d17cb428d47
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-11 16:09:29 +0530

    feat(nvim): disable terminal navigation keys

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          keys = {
+            nav_h = false,
+            nav_j = false,
+            nav_k = false,
+            nav_l = false,
+          },
+        },
+      },
+    },
+  },
+}

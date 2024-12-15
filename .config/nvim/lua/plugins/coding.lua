f9a377b958ef1d8ab6d5dd13125557764f302558
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-15 11:53:19 +0530

    feat(nvim/blink): enable supertab for snippets and completion

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -16,4 +16,25 @@ return {
       { "<leader>%", ":TSJToggle<CR>", desc = "args: Split or Join" },
     },
   },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<Tab>"] = {
+          function(cmp)
+            return cmp.snippet_active() and cmp.accept() or cmp.select_next()
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          function(cmp)
+            return cmp.snippet_active() and cmp.accept() or cmp.select_prev()
+          end,
+          "snippet_backward",
+          "fallback",
+        },
+      },
+    },
+  },
 }

3f3f4fa3954d4a2b103469dd642645a1fa9f3739
Harshad Nawathe <harshad.nawathe@gmail.com>
2024-12-30 10:36:20 +0530

    refactor(nvim): remove redundant configuration

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-vim.g.mapleader = " "
 vim.g.autoformat = false
 vim.g.loaded_netrwPlugin = 0
 vim.g.trouble_lualine = true

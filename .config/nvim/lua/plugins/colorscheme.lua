c4ea75f296f0ee33ba8ca886765eb7f07d72cc9c
Harshad Nawathe <harshad.nawathe@gmail.com>
2025-08-08 10:51:14 +0530

    feat(nvim): use tokyobones colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -34,9 +34,20 @@ return {
     },
   },
   {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "tokyobones",
     },
   },
 }

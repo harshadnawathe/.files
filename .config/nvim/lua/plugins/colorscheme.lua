dd92eda26f421dcde0effb93fb7135bfe0aaa9f6
Harshad Nawathe <harshad.nawathe@gmail.com>
2025-09-15 14:10:29 +0530

    feat(nvim): remove unused plugin transparent.nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,5 @@
 return {
   {
-    "xiyaowong/transparent.nvim",
-    lazy = false,
-    config = function()
-      local transparent = require("transparent")
-      transparent.setup({
-        extra_groups = {
-          "NormalFloat",
-          "NvimTreeNormal",
-        },
-      })
-      transparent.clear_prefix("BufferLine")
-      transparent.clear_prefix("NeoTree")
-      -- transparent.clear_prefix("lualine")
-    end,
-    keys = {
-      {
-        "<leader>uB",
-        "<Cmd>TransparentToggle<CR>",
-        desc = "Toggle background transparency",
-      },
-    },
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {

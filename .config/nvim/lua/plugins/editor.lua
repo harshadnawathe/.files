a476075d8399c8ab75cf7c3f36f8cf2fa462fab3
Harshad Nawathe <harshad.nawathe@gmail.com>
2025-12-14 17:13:59 +0530

    feat(nvim): configure snacks.nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "dinhhuy258/git.nvim",
-    event = "BufReadPre",
-    opts = {
-      default_mappings = false,
-    },
-    keys = {
-      { "<leader>gp", "<CMD>lua require('git.browse').pull_request()<CR>", desc = "Open pull request" },
-      { "<leader>gP", "<CMD>lua require('git.browse').create_pull_request()<CR>", desc = "Create new pull request" },
-    },
-  },
+
+  --   "dinhhuy258/git.nvim",
+  --   event = "BufReadPre",
+  --   opts = {
+  --     default_mappings = false,
+  --   },
+  --   keys = {
+  --     { "<leader>gp", "<CMD>lua require('git.browse').pull_request()<CR>", desc = "Open pull request" },
+  --     { "<leader>gP", "<CMD>lua require('git.browse').create_pull_request()<CR>", desc = "Create new pull request" },
+  --   },
+  -- },
   {
     "tpope/vim-rsi",
   },
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,35 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "norg", "svelte", "typst", "vue" } }, -- required for snacks.image
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = { ensure_installed = { "mmdc", "tectonic" } }, -- required for snacks.image
+  },
+  {
     "folke/snacks.nvim",
     opts = {
+      image = {
+        enabled = true,
+      },
+      statuscolumn = {
+        enabled = true,
+        folds = {
+          open = true,
+        },
+      },
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
       terminal = {
         win = {
           keys = {
@@ -11,6 +39,9 @@ return {
             nav_l = false,
           },
         },
+      },
+      words = {
+        enabled = true,
       },
     },
   },

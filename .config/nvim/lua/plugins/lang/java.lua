93a16b92e0c840881ee6d451cc6bca49351788fb
Harshad Nawathe <harshad.nawathe@gmail.com>
2025-12-14 17:13:33 +0530

    feat(nvim): use java 25 in jdtls plugin.

--- .config/nvim/lua/plugins/lang/java.lua
+++ .config/nvim/lua/plugins/lang/java.lua
@@ -4,7 +4,7 @@ return {
     ft = "java",
     opts = function(_, opts)
       -- java executable option for projects using JDK version older than 17
-      table.insert(opts.cmd, "--java-executable=" .. vim.fn.expand("$HOME/.local/share/mise/installs/java/21/bin/java"))
+      table.insert(opts.cmd, "--java-executable=" .. vim.fn.expand("$HOME/.local/share/mise/installs/java/25/bin/java"))
 
       return vim.tbl_deep_extend("force", opts, {
         -- custom on_attach hook to register additional options

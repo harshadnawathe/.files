1b03967163dac79b32e149700dc03ef531347edf
Harshad Nawathe <harshad.nawathe@gmail.com>
2026-05-21 22:22:13 +0530

    fix(fish): update command for brew upgrade and cleanup

--- .config/fish/conf.d/brew-abbr.fish
+++ .config/fish/conf.d/brew-abbr.fish
@@ -1,3 +1,3 @@
 abbr -a bcg 'brew outdated --greedy'
 abbr -a bcug 'brew upgrade --greedy'
-abbr -a bupc 'brew bundle check || brew bundle --all --cleanup; brew bundle cleanup --force; brew cleanup --prune=all; brew upgrade'
+abbr -a bupc 'brew update && brew bundle check || brew bundle --cleanup --force; brew upgrade; brew autoremove; brew cleanup --prune=0'

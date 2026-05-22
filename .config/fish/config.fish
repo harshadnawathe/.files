e50b0bb73d2c26eac85146a00344370582899f31
Harshad Nawathe <harshad.nawathe@gmail.com>
2026-05-22 11:40:52 +0530

    refactor(fish): rearrange the configuration based on interactive status.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,13 @@
+fish_add_path -gm \
+~/bin \
+~/go/bin \
+~/.local/bin \
+"/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
+
 if not status is-interactive
-    fish_add_path -gm ~/.local/share/mise/shims
-else
-    fzf_configure_bindings --directory=\cf --git_log= --git_status=\cs --history= --processes= --variables=\cv
+  fish_add_path -gm ~/.local/share/mise/shims
 end
 
-fish_add_path -gm \
-    ~/bin \
-    ~/go/bin \
-    ~/.local/bin \
-    "/Users/harshadn/Library/Application Support/JetBrains/Toolbox/scripts/"
+if status is-interactive
+  fzf_configure_bindings --directory=\cf --git_log= --git_status=\cs --history= --processes= --variables=\cv
+end

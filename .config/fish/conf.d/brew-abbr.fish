7be39db9538d5f63d8863197d1c473cb40c6aedb
Harshad Nawathe <harshad.nawathe@gmail.com>
2026-05-22 15:42:47 +0530

    feat(fish): remove bundle commands from bupc

--- .config/fish/conf.d/brew-abbr.fish
+++ .config/fish/conf.d/brew-abbr.fish
@@ -1,3 +1,3 @@
 abbr -a bcg 'brew outdated --greedy'
 abbr -a bcug 'brew upgrade --greedy'
-abbr -a bupc 'brew update && brew bundle check || brew bundle --cleanup --force; brew upgrade; brew autoremove; brew cleanup --prune=0'
+abbr -a bupc 'brew update && brew upgrade && brew cleanup; brew autoremove; brew cleanup --prune=0'

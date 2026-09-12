97d1d11d181561fc7db75bdff4a7aa3667346cde
Harshad Nawathe <harshad.nawathe@gmail.com>
2026-09-12 18:48:29 +0530

    chore: remove worktrunk

--- .config/fish/conf.d/misc-abbr.fish
+++ .config/fish/conf.d/misc-abbr.fish
@@ -2,10 +2,6 @@ abbr -a asf aws-secrets-fzf
 abbr -a !! --position anywhere --function last_command
 abbr -a tree ls --tree
 
-abbr -a wts wt switch
-abbr -a wtr wt remove
-abbr -a wtl wt list
-
 abbr -a wm workmux
 abbr -a wma workmux add
 abbr -a wmr workmux remove

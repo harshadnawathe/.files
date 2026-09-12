f8b3103f933ed8ace7162ff543c917abb4ee7e97
Harshad Nawathe <harshad.nawathe@gmail.com>
2026-09-12 18:46:12 +0530

    feat(tmux): install and configure workmux

--- .config/fish/conf.d/misc-abbr.fish
+++ .config/fish/conf.d/misc-abbr.fish
@@ -5,3 +5,10 @@ abbr -a tree ls --tree
 abbr -a wts wt switch
 abbr -a wtr wt remove
 abbr -a wtl wt list
+
+abbr -a wm workmux
+abbr -a wma workmux add
+abbr -a wmr workmux remove
+abbr -a wml workmux list
+abbr -a wmd workmux dashboard
+abbr -a wms workmux sidebar
